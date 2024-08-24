069ccf10c5d0e0a3f0c9d9f509967ddbb7d897bc
Fuksiarz <adrianbloch8@gmail.com>
2024-08-24 18:19:47 +0200

    dont fking ruszać tego

--- icer.com.pl/zaplecze/mainAPI.py
+++ icer.com.pl/zaplecze/mainAPI.py
@@ -1770,7 +1770,7 @@ def upload_predictor():
 
                 else:
                     # Informacja o braku kodu QR i przejście do predykcji jedzenia
-                    return jsonify({"status": "info", "message": "Nie wykryto kodu QR, przechodzę do klasyfikacji jedzenia."})
+                    logging("Nie wykryto kodu QR, przechodzę do klasyfikacji jedzenia.")
 
                 # Dokonanie predykcji na podstawie przesłanego obrazu
                 pred_class = pred_and_plot(model, file_path, class_names, username)
